3d9f0bf9f966e07a516a6e17afb282c258bd70be
Annisa Suci Soleha <annisasucisoleha5@gmail.com>
2024-11-06 18:52:21 +0700

    db sewabaru

--- project-truck-cooling/back-end/src/alat/sewaalat.js
+++ project-truck-cooling/back-end/src/alat/sewaalat.js
@@ -1,21 +1,24 @@
-import {response} from '../res/response.js'
+import { response } from '../res/response.js';
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString:
-    "postgresql://postgres:LBMHEDlIMcnMWMzOibdwsMSkSFmbbhKN@junction.proxy.rlwy.net:21281/railway", // Use the full connection string
+    "postgresql://postgres:LBMHEDlIMcnMWMzOibdwsMSkSFmbbhKN@junction.proxy.rlwy.net:21281/railway", // Gunakan connection string lengkap
 });
 
 export const getSewaAlat = async (req, res) => {
     try {
-      const { nama_calon_client } = req.body;
-      const newsewaAlat = await pool.query(
-        "INSERT INTO sewaAlat (nama_calon_client) VALUES($1)", [nama_calon_client]
+      const { nama_calon_client, no_telp } = req.body;  // Ambil nama dan no_telp dari request body
+
+      const newSewaAlat = await pool.query(
+        "INSERT INTO sewaAlat (nama_calon_client, no_whatsapp) VALUES($1, $2) RETURNING *",
+        [nama_calon_client, no_telp]
       );
 
-      res.json(sewaAlat);
-      } catch (err) {
-
-      }
-}
+      res.json(newSewaAlat.rows[0]);  // Mengembalikan hasil dari query
+    } catch (err) {
+      console.error(err.message);  // Menampilkan error di console
+      res.status(500).json({ error: "Terjadi kesalahan pada server" });
+    }
+};

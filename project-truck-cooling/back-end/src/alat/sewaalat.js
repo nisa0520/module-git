70d528efc548593d5b7c32212a6266ee37410fc3
Annisa Suci Soleha <annisasucisoleha5@gmail.com>
2024-11-13 13:17:42 +0700

    tambahan wa

--- project-truck-cooling/back-end/src/alat/sewaalat.js
+++ project-truck-cooling/back-end/src/alat/sewaalat.js
@@ -1,36 +1,47 @@
-import { response } from '../res/response.js';
-import pkg from "pg";
-const { Pool } = pkg;
+// import pkg from "pg";
+// const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString:
-    "postgresql://postgres:LBMHEDlIMcnMWMzOibdwsMSkSFmbbhKN@junction.proxy.rlwy.net:21281/railway",
-});
+// const pool = new Pool({
+//   connectionString:
+//     "postgresql://postgres:LBMHEDlIMcnMWMzOibdwsMSkSFmbbhKN@junction.proxy.rlwy.net:21281/railway",
+// });
 
-// Endpoint untuk mengambil data dari tabel sewaAlat
-export const getSewaAlat = async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM sewaAlat");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ error: "Terjadi kesalahan pada server" });
-  }
-};
+// // Endpoint untuk mengambil semua data dari tabel sewaAlat
+// export const getSewaAlat = async (req, res) => {
+//   try {
+//     const result = await pool.query("SELECT * FROM sewaAlat");
+//     res.status(200).json(result.rows);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).json({ error: "Terjadi kesalahan pada server" });
+//   }
+// };
 
-// Endpoint untuk menambahkan data ke tabel sewaAlat
-export const addSewaAlat = async (req, res) => {
-  try {
-    const { nama_calon_client, no_telp } = req.body;
+// // Endpoint untuk menambahkan data ke tabel sewaAlat
+// export const addSewaAlat = async (req, res) => {
+//   const { nama_calon_client, no_whatsapp } = req.body;
+//   try {
+//     const newSewaAlat = await pool.query(
+//       "INSERT INTO sewaAlat (nama_calon_client, no_whatsapp) VALUES ($1, $2) RETURNING *",
+//       [nama_calon_client, no_whatsapp]
+//     );
+//     res.status(201).json(newSewaAlat.rows[0]); 
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).json({ error: "Terjadi kesalahan pada server" });
+//   }
+// };
 
-    const newSewaAlat = await pool.query(
-      "INSERT INTO sewaAlat (nama_calon_client, no_whatsapp) VALUES($1, $2) RETURNING *",
-      [nama_calon_client, no_telp]
-    );
 
-    res.json(newSewaAlat.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ error: "Terjadi kesalahan pada server" });
-  }
-};
+// export const createSewaAlat = async (nama_calon_client, no_whatsapp) => {
+//   try {
+//     const result = await pool.query(
+//       "INSERT INTO sewaAlat (nama_calon_client, no_whatsapp) VALUES ($1, $2) RETURNING *",
+//       [nama_calon_client, no_whatsapp]
+//     );
+//     return result.rows[0]; // Kembalikan data yang baru dimasukkan sebagai respon
+//   } catch (err) {
+//     console.error(err.message);
+//     throw new Error("Gagal membuat data sewaAlat");
+//   }
+// };
